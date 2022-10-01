f3f37605b4727c017124fe3deb93d045ffa994b5
vbasoon <vbasoon@gmail.com>
2022-10-01 21:20:20 +0300

    first msg

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: ["plugin:vue/vue3-essential", "eslint:recommended"],
+  parserOptions: {
+    parser: "@babel/eslint-parser",
+  },
+  rules: {
+    "no-unused-vars": process.env.NODE_ENV == "production" ? "error" : "warn",
+  },
+  parser: "vue-eslint-parser",
+};
