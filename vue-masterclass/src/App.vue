e440fefadb5bff2b2b73957e2cb46364f184bc98
vbasoon <vbasoon@gmail.com>
2022-10-08 09:06:52 +0300

    vue-router

--- vue-masterclass/src/App.vue
+++ vue-masterclass/src/App.vue
@@ -1,6 +1,6 @@
 <template>
+  <img alt="Vue logo" src="@/assets/logo.png" />
   <div class="container">
-    <img alt="Vue logo" src="./assets/logo.png" />
     <home-page />
   </div>
 </template>
@@ -8,7 +8,7 @@
 <script>
 //import HelloWorld from "./components/HelloWorld.vue";
 import HomePage from "@/components/HomePage.vue";
-const name = "alex";
+//const name = "alex";
 
 export default {
   name: "App",
@@ -20,12 +20,6 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import "assets/style.css";
+/* @import "~bootstrap/css/bootstrap.css"; */
 </style>
---
+++ vue-masterclass/src/components/HomePage copy.vue
@@ -0,0 +1,56 @@
+<template>
+  <div v-for="thread in threads" :key="thread.id" class="col-large push-top">
+    <h2>Threads:{{ thread.title }}</h2>
+    <div class="post-list">
+      <div class="post" v-for="postId in thread.posts" :key="postId">
+        <div class="user-info">
+          <a href="#" class="user-name">{{
+            userById(postById(postId).userId).name
+          }}</a>
+          <a href="#">
+            <img
+              class="avatar-large"
+              :src="userById(postById(postId).userId).avatar"
+              alt="avatar"
+            />
+          </a>
+
+          <p class="stats__posts">count_posts</p>
+        </div>
+        <div class="post__content">
+          <div>
+            <p>
+              {{ postById(postId).text }}
+            </p>
+          </div>
+        </div>
+        <div class="post-date text-faded">
+          {{ postById(postId).publishedAt }}
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import sourceData from "@/data.json";
+export default {
+  data() {
+    return {
+      threads: sourceData.threads,
+      posts: sourceData.posts,
+      users: sourceData.users,
+    };
+  },
+  methods: {
+    postById(postId) {
+      return this.posts.find((p) => p.id === postId);
+    },
+    userById(userId) {
+      return this.users.find((p) => p.id === userId);
+    },
+  },
+};
+</script>
+
+<style></style>
